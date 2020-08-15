dc1e6ef88b7abc41f3ef7df7dca2127b60ad7882
azam-at-017373302025 <azamrajabov@gmail.com>
2020-08-15 08:53:08 -0500

    enabled sentry

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
 from flask import render_template
 from app import app
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+
+sentry_sdk.init(
+    dsn="https://ef289e49683a40e2b5ad86e8e9f5d91e@sentry.io/2727567",
+    integrations=[FlaskIntegration()]
+)
+
 
 @app.route('/')
 @app.route('/index')
