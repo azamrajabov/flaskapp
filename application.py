bdf36f41c841561786d8d5db27759c1f53b167db
azam-at-017373302025 <azamrajabov@gmail.com>
2020-08-15 18:55:27 -0500

    fixes

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from app import app
 
-application = Flask(__name__)
+application = app
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
