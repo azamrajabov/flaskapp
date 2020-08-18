a6c101d5127187623305115dbde83d413702a379
azam-at-017373302025 <azamrajabov@gmail.com>
2020-08-18 02:02:03 -0500

    microblog to 2go replaces

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,7 @@ if not app.debug:
         # mail_handler = SMTPHandler(
         #     mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
         #     fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-        #     toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+        #     toaddrs=app.config['ADMINS'], subject='2GoNYC app Failure',
         #     credentials=auth, secure=secure)
         # mail_handler.setLevel(logging.ERROR)
         # app.logger.addHandler(mail_handler)
@@ -50,4 +50,4 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.logger.info('2GoNYC app startup')
